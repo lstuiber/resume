5a9c7525c0681bcb0b5cdf2696d0ce8908d1cc26
lstuiber <lucasstuiber@gmail.com>
2019-02-01 13:06:46 -0600

    Home page 90% completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { EducationComponent } from './education/education.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WorkComponent,
+    EducationComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+ 
+  constructor() { 
+  }
+ fade(id){
+ var element = document.getElementById(id);
+ element.style.display = "block";
+ element.style.opacity = '0';
+ element.style.animation = '2s ease 0s normal forwards 1 fadein';
+ 
+}
   ngOnInit() {
   }
 
